affa920a6bfa3405b8946d2e59aeafb30c689595
KSL78 <53367924+KSL78@users.noreply.github.com>
2022-10-27 01:04:47 +0900

    Update protov0.ino

--- protov0/protov0.ino
+++ protov0/protov0.ino
@@ -8,82 +8,116 @@ const int stepv = 512;//각도 90도
 Stepper mStepper(stepv,11,9,10,8);
 int a=0,b=0,eds = 0;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+class Stcerov
+{
+  private:
+    int str=0;
+  public:
+    Stcerov(int vam1){
+      str=vam1;    
+    }
+    void prtc();
+};
+void Stcerov::prtc(){
+  if (str == 1)
+  {
+    digitalWrite(5,HIGH);
+    digitalWrite(6,HIGH);
+    delay(1000);
+    digitalWrite(5,LOW);
+    digitalWrite(6,LOW);
+    eds=1;
+    //"카메라의 데이터가 전송되지 않습니다. 카메라 전원을 확인하여 주십시오."
+  }
+  else if (str == 2)
+  {
+    digitalWrite(5,HIGH);
+    digitalWrite(7,HIGH);
+    delay(1000);
+    digitalWrite(5,LOW);
+    digitalWrite(7,LOW);
+    eds=2;
+    //"데이터에 없습니다. 얼굴을 인식하여 주십시오."
+  }
+  else if (str == 5)
+  {
+    digitalWrite(7,HIGH);
+    delay(1000);
+    digitalWrite(7,LOW);
+  }
+}
 void setup() {
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);   
-  lcd.print("pplayer :");
-  lcd.setCursor(0,1);
-  lcd.print("nplayer :");
   mStepper.setSpeed(28);//속도
   Serial.begin(9600);
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
+  pinMode(5,OUTPUT);//r
+  pinMode(6,OUTPUT);//g
+  pinMode(7,OUTPUT);//b
   Wire.begin();
   while(!huskylens.begin(Wire))
   {
-    Serial.println(F("Begin failed"));
+    Stcerov bval(5);
+    bval.prtc();
+    eds=5;
     delay(100);
   }
 }
-
 void loop() {
   lcd.setCursor(0,0);   
-  lcd.print("pplayer :");
+  lcd.print("pp:");
   lcd.setCursor(0,1);
-  lcd.print("nplayer :");
-  lcd.setCursor(10,0);           
+  lcd.print("np:");
+  lcd.setCursor(3,0);           
   lcd.print(a);
-  lcd.setCursor(10,1);           
-  lcd.print(b); 
+  lcd.setCursor(3,1);           
+  lcd.print(b);
+  lcd.setCursor(9,0);   
+  lcd.print("ec:");
+  lcd.setCursor(12,0);   
+  lcd.print(eds);
+  lcd.setCursor(9,1);   
+  lcd.print("p:");
+  lcd.setCursor(12,1);   
+  lcd.print("0");
   if (huskylens.available())
   {
     HUSKYLENSResult result = huskylens.read();
     mtmove(result);
+    eds=0;
   }
-  else
+  else if (!huskylens.request())
   {
-    edst();
+    Stcerov req(1);
+    req.prtc();
   }
-  
+  else if (!huskylens.available()) 
+  {
+    Stcerov aval(2);
+    aval.prtc();
+  } 
   delay(1000);
 }
 void mtmove(HUSKYLENSResult result)
 {
   if (result.command == COMMAND_RETURN_BLOCK) {
-    Serial.println(String()+F("ID NUMBER:")+result.ID);
     if (result.ID == 1 || result.ID == 2) {
+      lcd.setCursor(12,1);   
+      lcd.print(result.ID);
       digitalWrite(6,HIGH);
-      mStepper.step(stepv);
-         
+      digitalWrite(7,HIGH);
+      mStepper.step(stepv);  
       delay(6000);        
       mStepper.step(-stepv);
       digitalWrite(6,LOW);
-      a=a+1;
-      lcd.clear();
+      digitalWrite(7,LOW);
+      a=a+1; 
     }
     else {
-      digitalWrite(7,HIGH);
+      digitalWrite(5,HIGH);
       delay(1000);
-      digitalWrite(7,LOW);
+      digitalWrite(5,LOW);
       b=b+1;
-      lcd.clear();
     }
   }
-
 }
-void edst()
-{
-  if (!huskylens.request()) natvque();
-  else if (!huskylens.available()) natvavali();
-}
-
-void natvque(){
-  
-  Serial.println(F("카메라의 데이터가 전송되지 않습니다. 카메라 전원을 확인하여 주십시오."));
-  
-}
-void natvavali(){
-  Serial.println(F("데이터에 없습니다. 얼굴을 인식하여 주십시오."));
-}
-// 객체로 쓸수 있었는데 아깝다.......
